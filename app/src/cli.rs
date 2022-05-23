f9d749129a90564c417d575200df1eeedd9e3593
Mike the Tike <mikethetike@tari.com>
2022-05-23 10:40:17 +0200

    docs: add tor cli doc

--- app/src/cli.rs
+++ app/src/cli.rs
@@ -8,6 +8,7 @@ pub fn init() -> Cli {
 #[clap(author, version, about, long_about = None)]
 #[clap(propagate_version = true)]
 pub struct Cli {
+    /// Use a local instance of Tor instead of starting one. Tari usually uses 9051
     #[clap(short = 't', long, alias = "local-tor-port")]
     pub local_tor_control_port: Option<u16>,
 }
